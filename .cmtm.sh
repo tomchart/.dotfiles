e7118fe8f90f0a63ca99c7989dd61b97d26623d0
Tom Hart <tom@reventus.co.uk>
2021-08-24 17:26:22 +0100

    fix cmtm after changing pane numbering

--- .cmtm.sh
+++ .cmtm.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 tmux new-session -d
 tmux split-window -v
-tmux select-pane -t 1
-tmux split-window -h
-tmux select-pane -t 0
-tmux split-window -h
-tmux select-pane -t 0
-tmux send-keys 'docker container logs trackurls -f' C-m
-tmux select-pane -t 1
-tmux send-keys 'docker container logs updatestatus -f' C-m
 tmux select-pane -t 2
-tmux send-keys 'docker container logs deletefiles -f' C-m
+tmux split-window -h
+tmux select-pane -t 1
+tmux split-window -h
+tmux select-pane -t 1
+tmux send-keys 'docker container logs trackurls -f' C-m
+tmux select-pane -t 2
+tmux send-keys 'docker container logs updatestatus -f' C-m
 tmux select-pane -t 3
+tmux send-keys 'docker container logs deletefiles -f' C-m
+tmux select-pane -t 4
 tmux send-keys 'docker container logs sendmessages -f' C-m
 tmux attach
