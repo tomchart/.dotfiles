b418da4adff8e4c8a89e193c512b1758f5035545
Tom Hart <tom@reventus.co.uk>
2021-11-18 14:54:46 +0000

    begin moving to lua vim

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Making symlinks..."
 ln -isv ~/dotfiles/.cmtm.sh ~
 ln -isv ~/dotfiles/.gitconfig ~
 ln -isv ~/dotfiles/.tmux.conf ~
-ln -isv ~/dotfiles/init.vim ~/.config/nvim
+ln -isv ~/dotfiles/init.lua ~/.config/nvim
 exit
 
 echo "Installing zsh, tmux, neovim..."
