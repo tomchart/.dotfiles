a68c2787007dda00960a2fdc3af324a2f223a803
Tom Hart <tom@reventus.co.uk>
2021-11-19 16:49:13 +0000

    add gitgutter

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -6,33 +6,38 @@ cmd('autocmd BufWritePost packer.lua source <afile> | PackerCompile')
 return require('packer').startup(function()
             use 'wbthomason/packer.nvim'
 
-            -- UI
-            use 'machakann/vim-highlightedyank'
+        -- UI
+        use 'machakann/vim-highlightedyank'
+        use 'lukas-reineke/indent-blankline.nvim'
+        use 'glepnir/dashboard-nvim'
+        use 'airblade/vim-gitgutter'
+        use {
+                'airblade/vim-gitgutter',
+                config = [[ require 'plugins.vim_gitgutter' ]],
+        }
 
-            -- Command
-            use 'justinmk/vim-sneak'
-            use 'tpope/vim-eunuch'
-            use 'tpope/vim-commentary'
-            use 'tpope/vim-surround'
-            use 'tpope/vim-fugitive'
-            use 'lukas-reineke/indent-blankline.nvim'
-            use 'glepnir/dashboard-nvim'
-            use {
+        -- Command
+        use 'justinmk/vim-sneak'
+        use 'tpope/vim-eunuch'
+        use 'tpope/vim-commentary'
+        use 'tpope/vim-surround'
+        use 'tpope/vim-fugitive'
+        use {
                 'bkad/CamelCaseMotion',
                 config = [[ require 'plugins.camelcasemotion' ]]
-            }
-            use {
+        }
+        use {
                 'kyazdani42/nvim-tree.lua',
                 config = [[ require 'plugins.nvim_tree' ]],
                 requires = 'kyazdani42/nvim-web-devicons',
-            }
-            use {
+        }
+        use {
                 'nvim-telescope/telescope.nvim',
                 requires = { {'nvim-lua/plenary.nvim'} }
-            }
+        }
 
-            -- Theme
-            use {'dracula/vim', as = 'dracula'}
+        -- Theme
+        use {'dracula/vim', as = 'dracula'}
 end)
 
 
