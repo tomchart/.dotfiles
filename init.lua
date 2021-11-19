41a4ab3016ac0e1371c46d62c3593656734fd55d
Tom Hart <tom@reventus.co.uk>
2021-11-19 10:11:01 +0000

    start adding mappings

--- init.lua
+++ init.lua
@@ -7,9 +7,27 @@ cmd('autocmd BufWritePost init.lua source <afile>')
 require 'plugins.packer'
 require 'mappings'
 
+-----------------------------------------------------
+-- theme/colours
+-----------------------------------------------------
 cmd('packadd dracula')
 cmd('colorscheme dracula')
 
+
+-----------------------------------------------------
 -- options
+-----------------------------------------------------
 o.smartcase = true
+o.ignorecase = true
 o.termguicolors = true
+o.cursorline = true
+
+
+-----------------------------------------------------
+-- misc
+-----------------------------------------------------
+-- jump to last position when opening a file
+cmd([[autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
+
+
+
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,7 @@ cmd('autocmd BufWritePost mappings.lua source <afile>')
 
 g.mapleader = ' '
 
--- nicked this nice func from marcus
+-- nicked this func from marcus
 local function map(mode, lhs, rhs, opts)
   local options = {
     noremap = true,
@@ -18,8 +18,36 @@ local function map(mode, lhs, rhs, opts)
   api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+
+-------------------------------------------------------
+-- moving around, tabs, windows, and buffers
+-------------------------------------------------------
+-- in insert mode, jj exits this mode
 map('i', 'jj', '<esc>')
 
+-- <leader><cr> hides highlight
+map('n', '<leader><cr>', ':noh<cr>')
+
+-- some comfort binds
+-- thanks again marcus, these are tight as fuck
+map('n', 'gk', 'gg')
+map('v', 'gk', 'gg')
+map('n', 'gj', 'G')
+map('v', 'gj', 'G')
+map('n', 'gh', '^')
+map('v', 'gh', '^')
+map('n', 'gl', '$')
+map('v', 'gl', 'g_')
+
+-- copy to end of line from current pos with Y
+map('n', 'Y', 'y$')
+
+
+
+-------------------------------------------------------
+-- plugin mappings
+-------------------------------------------------------
 -- nvim-tree.lua
 map('n', '<c-n>', '<cmd>NvimTreeToggle<cr>')
+
 
