8e4f8aa39040f7dda51d1f490335c722e57e26f0
Tom Hart <tom@reventus.co.uk>
2021-11-29 16:09:33 +0000

    add auto install for packer to packer lua

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,5 +1,13 @@
 local g = vim.g
 local cmd = vim.cmd
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local execute = vim.api.nvim_command
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
 
 cmd('autocmd BufWritePost packer.lua source <afile> | PackerCompile')
 
