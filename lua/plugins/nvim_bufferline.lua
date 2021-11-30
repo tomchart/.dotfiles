e6448220112d21a103e579b5778e586e963932de
Tom Hart <tom@reventus.co.uk>
2021-11-30 16:50:32 +0000

    add bufferline config

--- lua/plugins/nvim_bufferline.lua
+++ lua/plugins/nvim_bufferline.lua
@@ -1,19 +1,41 @@
-require('bufferline').setup{
-  highlights = {
-    buffer_selected = {
-      gui = 'bold',
-    },
-    duplicate_selected = {
-      gui = 'bold',
-    },
-    duplicate = {
-      gui = '',
-    },
-  },
-  options = {
-    max_name_length = 30,
-    tab_size = 12,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-  },
+local bufferline = require'bufferline'
+
+bufferline.setup{
+        highlights = {
+                buffer_selected = {
+                        gui = 'bold',
+                },
+                duplicate_selected = {
+                        gui = 'bold',
+                },
+                duplicate = {
+                        gui = '',
+                },
+        },
+        options = {
+                middle_mouse_command = "bdelete! %d",
+                indicator_icon = '▎',
+                buffer_close_icon = '',
+                modified_icon = '●',
+                close_icon = '',
+                left_trunc_marker = '',
+                right_trunc_marker = '',        
+                max_name_length = 30,
+                max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+                tab_size = 18,
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                        local s = " "
+                        for e, n in pairs(diagnostics_dict) do
+                                local sym = e == "error" and " "
+                                or (e == "warning" and " " or " " )
+                                s = s .. n .. sym
+                        end
+                        return s
+                end,
+                show_buffer_close_icons = true,
+                separator_style = "slant",
+                offsets = {{filetype = "NvimTree", text = "File Explorer"}} 
+
+        },
 }
