452f018a0a0aaee7758141ae6b00733ca2b148f5
Tom Hart <tom@reventus.co.uk>
2021-11-30 16:50:45 +0000

    add more lspconfig stuff

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -1,1 +1,26 @@
-require'lspconfig'.pyright.setup{}
+local lspconfig = require'lspconfig'
+local fn = vim.fn
+
+lspconfig.pyright.setup{
+  flags = {
+    debounce_text_changes = 150,
+  },
+  root_dir = function(fname)
+    return fn.getcwd()
+  end,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true,
+       },
+      },
+    },
+  }
+
+lspconfig.bashls.setup{}
+lspconfig.dockerls.setup{}
+lspconfig.html.setup{}
+
+lspconfig.sqlls.setup{}
