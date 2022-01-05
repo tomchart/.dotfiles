342e953f0aff0b2648ab9b5c8d246ea60bb1317a
Tom Hart <tom@reventus.co.uk>
2022-01-05 14:15:26 +0000

    some changes to init.lua to require external files for additional settings

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ cmd('autocmd BufWritePost init.lua source <afile>')
 
 require 'plugins.packer'
 require 'mappings'
+require 'options'
+require 'colours'
 
 -----------------------------------------------------
 -- theme/colours
@@ -15,29 +17,8 @@ cmd('colorscheme dracula')
 
 
 -----------------------------------------------------
--- options
------------------------------------------------------
-o.expandtab = true
-o.mouse = 'a'
-o.smartcase = true
-o.ignorecase = true
-o.termguicolors = true
-o.cursorline = true
-o.swapfile = false
-o.splitbelow = true
-o.splitright = true
-o.showtabline = 2
-o.tabstop = 4
-o.number = true
-vim.wo.colorcolumn = "99999"    -- fix annoying indent artifacts bug
--- vim.lsp.set_log_level("debug")
-
-
------------------------------------------------------
 -- misc
 -----------------------------------------------------
 -- jump to last position when opening a file
 cmd([[autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
-
-
 
