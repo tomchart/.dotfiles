b720a3092c78f622c0212176d3ccecebca548a9d
Tom Hart <tom@reventus.co.uk>
2022-01-18 16:21:30 +0000

    add dracula zsh theme

--- install.sh
+++ install.sh
@@ -31,6 +31,10 @@ echo "Installing Oh My Zsh plugins: fast-syntax-highlighting, zsh-autosuggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "Installing Oh My Zsh dracula theme..."
+git clone https://github.com/dracula/zsh.git ${ZSH}/themes/dracula
+ln -isv ${ZSH}/themes/dracula/dracula.zsh-theme ${ZSH}/themes/dracula.zsh-theme
+
 echo "Installing tmux plugin manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
