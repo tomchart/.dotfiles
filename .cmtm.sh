67c0b0fe68f317e63af786d0ba044eefa6942fe7
Tom Hart <tom@reventus.co.uk>
2022-02-03 15:11:04 +0000

    add denv to cmtm sh

--- .cmtm.sh
+++ .cmtm.sh
@@ -6,11 +6,11 @@ tmux split-window -h
 tmux select-pane -t 1
 tmux split-window -h
 tmux select-pane -t 1
-tmux send-keys 'docker container logs trackurls -f' C-m
+tmux send-keys 'denv && docker container logs trackurls -f' C-m
 tmux select-pane -t 2
-tmux send-keys 'docker container logs updatestatus -f' C-m
+tmux send-keys 'denv && docker container logs updatestatus -f' C-m
 tmux select-pane -t 3
-tmux send-keys 'docker container logs deletefiles -f' C-m
+tmux send-keys 'denv && docker container logs deletefiles -f' C-m
 tmux select-pane -t 4
-tmux send-keys 'docker container logs sendmessages -f' C-m
+tmux send-keys 'denv && docker container logs sendmessages -f' C-m
 tmux attach
