cd3530792e0d214fa151ecd08df7ad1696e7b934
Tom Hart <tom@reventus.co.uk>
2022-02-15 16:46:05 +0000

    add indent markers to nvimtree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -4,6 +4,7 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 g.nvim_tree_quit_on_open = 0
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
+g.nvim_tree_indent_markers = 1
 
 require("nvim-tree").setup({
 	auto_close = 0,
