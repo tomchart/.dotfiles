1b74fa42c6d58f9466ea1d7ea6836c80db2276b4
Tom Hart <tom@reventus.co.uk>
2022-02-21 15:12:34 +0000

    format cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,6 @@
-local cmp = require 'cmp'
-local lspkind = require 'lspkind'
-local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local o = vim.o
 local fn = vim.fn
 local api = vim.api
@@ -8,14 +8,14 @@ local api = vim.api
 -- this whole lua file is stolen from marcus p much
 -- shoutout the real og one time
 
-o.shortmess = o.shortmess .. 'c'
+o.shortmess = o.shortmess .. "c"
 
 local function replace_termcodes(s)
-  return api.nvim_replace_termcodes(s, false, false, true)
+	return api.nvim_replace_termcodes(s, false, false, true)
 end
 
 local function feedkeys(keys, mode)
-  api.nvim_feedkeys(replace_termcodes(keys), mode, true)
+	api.nvim_feedkeys(replace_termcodes(keys), mode, true)
 end
 
 -- Check if we should tab out of a pair of brackets / quotes. Returns true if
@@ -23,65 +23,64 @@ end
 -- - closing bracket
 -- - quote and we're inside a pair of them
 local function should_tab_out()
-  local brackets = {
-    [')'] = true,
-    [']'] = true,
-    ['}'] = true,
-  }
-  local quotes = {
-    ["'"] = true,
-    ['"'] = true,
-    ['`'] = true
-  }
+	local brackets = {
+		[")"] = true,
+		["]"] = true,
+		["}"] = true,
+	}
+	local quotes = {
+		["'"] = true,
+		['"'] = true,
+		["`"] = true,
+	}
 
-  local line = fn.getline('.')
-  local col = fn.col('.')
-  local next_char = line:sub(col, col)
+	local line = fn.getline(".")
+	local col = fn.col(".")
+	local next_char = line:sub(col, col)
 
-  if quotes[next_char] then
-    local preceding_chars = line:sub(1, col - 1)
-    num_preceding_quotes = select(2, preceding_chars:gsub(next_char, ''))
-    -- if odd number of preceding quotes, then we're currently inside a pair
-    return num_preceding_quotes % 2 == 1
-  end
-  return brackets[next_char] == true
+	if quotes[next_char] then
+		local preceding_chars = line:sub(1, col - 1)
+		num_preceding_quotes = select(2, preceding_chars:gsub(next_char, ""))
+		-- if odd number of preceding quotes, then we're currently inside a pair
+		return num_preceding_quotes % 2 == 1
+	end
+	return brackets[next_char] == true
 end
 
 cmp.setup({
-    sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'vsnip' },
-    }, {
-        { name = 'buffer' }
-    }),
-    snippet = {
-        expand = function(args)
-            fn['vsnip#anonymous'](args.body)
-        end,
-    },
-    confirmation = {
-        default_behavior = cmp.ConfirmBehavior.Replace,
-    },
-    mapping = {
-        ['<c-space>'] = cmp.mapping.complete(),
-        ['<Tab>'] = function(fallback)
-        if cmp.visible() then
-            cmp.close()
-        elseif should_tab_out() then
-            feedkeys('<right>', 'i')
-        else
-            fallback()
-            end
-        end,
-        ['<cr>'] = cmp.mapping.confirm({select = true}),
-    },
-    formatting = {
-        format = lspkind.cmp_format({with_text = false, maxwidth = 50})
-    },
-    experimental = {
-        ghost_text = true,
-    },
-}
-)
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+	snippet = {
+		expand = function(args)
+			fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	confirmation = {
+		default_behavior = cmp.ConfirmBehavior.Replace,
+	},
+	mapping = {
+		["<c-space>"] = cmp.mapping.complete(),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.close()
+			elseif should_tab_out() then
+				feedkeys("<right>", "i")
+			else
+				fallback()
+			end
+		end,
+		["<cr>"] = cmp.mapping.confirm({ select = true }),
+	},
+	formatting = {
+		format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
+	},
+	experimental = {
+		ghost_text = true,
+	},
+})
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
