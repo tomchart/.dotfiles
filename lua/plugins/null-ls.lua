b69123ceb93428fa601a22ffae4d3dae4e477173
Tom Hart <tom@reventus.co.uk>
2022-02-22 11:54:41 +0000

    some stuff

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,6 +2,7 @@ local null_ls = require("null-ls")
 
 local sources = {
 	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.sqlformat,
 	null_ls.builtins.diagnostics.flake8,
 	null_ls.builtins.diagnostics.mypy,
 }
