f0508161163a089722dae37d1f767dfed32ef452
Tom Hart <tom@reventus.co.uk>
2022-02-22 15:55:54 +0000

    add some space to line blame text

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -40,5 +40,5 @@ require("gitsigns").setup({
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	current_line_blame_formatter = "        <author>, <author_time:%Y-%m-%d> - <summary>",
 })
