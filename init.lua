f6ef18092f06bf42217b014e9855552dbeb38346
Tom Hart <tom@reventus.co.uk>
2022-02-23 10:17:18 +0000

    start rewriting init

--- init.lua
+++ init.lua
@@ -1,6 +1,27 @@
-local cmd = vim.cmd
+local present, impatient = pcall(require, "impatient")
 
-cmd("autocmd BufWritePost init.lua source <afile>")
+if present then
+	impatient.enable_profile()
+end
+
+local core_modules = {
+	"core.mappings",
+}
+
+for _, module in ipairs(core_modules) do
+	local ok, err = pcall(require, module)
+	if not ok then
+		error("Error loading " .. module .. "\n\n" .. err)
+	end
+end
+
+require("core.mappings").misc()
+
+-- require("core.mappings").nvimtree()
+-- require("core.mappings").tele()
+-- require("core.mappings").bufferline()
+-- require("core.mappings").lspconf()
+-- require("core.mappings").trouble()
 
 require("mappings")
 require("options")
