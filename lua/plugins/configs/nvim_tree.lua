31ab2505307cff876fe84d7fe7b8796526677c9f
Tom Hart <tom@reventus.co.uk>
2022-02-24 09:49:27 +0000

    add lazy config for nvimtree

--- lua/plugins/configs/nvim_tree.lua
+++ lua/plugins/configs/nvim_tree.lua
@@ -1,3 +1,9 @@
+local present, nvimtree = pcall(require, "nvim-tree")
+
+if not present then
+	return
+end
+
 local g = vim.g
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
@@ -6,11 +12,19 @@ g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
 g.nvim_tree_indent_markers = 1
 
-require("nvim-tree").setup({
+local default = {
 	auto_close = 0,
 	view = {
 		auto_resize = true,
 		width = 40,
 	},
 	git_hl = 1,
-})
+}
+
+local M = {}
+
+M.setup = function()
+	nvimtree.setup(default)
+end
+
+return M
