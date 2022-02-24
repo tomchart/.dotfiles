b60c618f63b1cea8b2a7a920c73470a855db7100
Tom Hart <tom@reventus.co.uk>
2022-02-24 09:49:04 +0000

    add lazy config for autopairs

--- lua/plugins/configs/nvim_autopairs.lua
+++ lua/plugins/configs/nvim_autopairs.lua
@@ -1,1 +1,13 @@
-require("nvim-autopairs").setup({})
+local present, autopairs = pcall(require, "nvim-autopairs")
+
+if not present then
+	return
+end
+
+local M = {}
+
+M.setup = function()
+	autopairs.setup({})
+end
+
+return M
