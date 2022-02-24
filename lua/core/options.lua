94c8ff7fa7405c9158d944aa5ee5d4387012e500
Tom Hart <tom@reventus.co.uk>
2022-02-24 10:52:58 +0000

    disable builtin plugins in options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -21,3 +21,29 @@ o.updatetime = 100
 o.scrolloff = 5
 vim.wo.colorcolumn = "99999" -- fix annoying indent artifacts bug
 -- vim.lsp.set_log_level("debug")
+
+-- disable some builtin vim plugins
+local disabled_built_ins = {
+	"2html_plugin",
+	"getscript",
+	"getscriptPlugin",
+	"gzip",
+	"logipat",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"matchit",
+	"tar",
+	"tarPlugin",
+	"rrhelper",
+	"spellfile_plugin",
+	"vimball",
+	"vimballPlugin",
+	"zip",
+	"zipPlugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+	vim.g["loaded_" .. plugin] = 1
+end
