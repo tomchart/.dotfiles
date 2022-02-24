b6c8b300616b5ee7f6c78d0490be0c4cfde517c6
Tom Hart <tom@reventus.co.uk>
2022-02-24 16:47:00 +0000

    manually add cmp capabilities to lspconfig so dont have to require cmp in this config file

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 local present, lspconfig = pcall(require, "lspconfig")
 if not present then
-    return
+	return
 end
 
 local fn = vim.fn
@@ -101,8 +101,22 @@ local servers = {
 	-- },
 }
 
--- local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		"documentation",
+		"detail",
+		"additionalTextEdits",
+	},
+}
 for server, config in pairs(servers) do
 	lspconfig[server].setup({
 		capabilities = capabilities,
