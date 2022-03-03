670837353e2391ce493dd60d8c3ddbf1ba8397ab
Tom Hart <tom@reventus.co.uk>
2022-03-03 11:26:41 +0000

    amend nvimtree config

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -5,9 +5,7 @@ if not present then
 end
 
 local g = vim.g
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
 g.nvim_tree_indent_markers = 1
