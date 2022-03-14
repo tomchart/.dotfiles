cf1ae5ae607774f4a8d939ce6e8a5107021c13a5
Tom Hart <tom@reventus.co.uk>
2022-03-14 12:18:35 +0000

    add cmp luasnip config in others

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -46,4 +46,16 @@ M.escape = function()
 	})
 end
 
+M.luasnip = function()
+	local present, luasnip = pcall(require, "luasnip")
+	if present then
+		local default = {
+			history = true,
+			updateevents = "TextChanged,TextChangedI",
+		}
+		luasnip.config.set_config(default)
+		require("luasnip/loaders/from_vscode").load()
+	end
+end
+
 return M
