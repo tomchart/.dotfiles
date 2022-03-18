7b42e9cf4f5bb6b7e687054c2f51abcc3d6538b6
Tom Hart <tom@reventus.co.uk>
2022-03-18 10:01:07 +0000

    add icons to cmp

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -4,8 +4,33 @@ if not present then
 	return
 end
 
--- this whole lua file is stolen from marcus p much
--- shoutout the real og one time
+local icons = {
+	Text = "",
+	Method = "",
+	Function = "",
+	Constructor = "",
+	Field = "ﰠ",
+	Variable = "",
+	Class = "ﴯ",
+	Interface = "",
+	Module = "",
+	Property = "ﰠ",
+	Unit = "塞",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "פּ",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+}
 
 vim.o.shortmess = vim.o.shortmess .. "c"
 
