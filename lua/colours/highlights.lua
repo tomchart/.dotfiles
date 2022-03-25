9ac810f39a2d032361b2792c7a47fe96b63c3292
Tom Hart <tom@reventus.co.uk>
2022-03-25 16:32:47 +0000

    some stuff in highlights, also kanagawa stuff

--- lua/colours/highlights.lua
+++ lua/colours/highlights.lua
@@ -6,31 +6,115 @@ local fg_bg = require("core.utils").fg_bg
 local extract_highlight_colours = require("core.utils").extract_highlight_colours
 local highlight_group = require("core.utils").highlight_group
 
+local nord = {
+	white = "#abb2bf",
+	darker_black = "#2a303c",
+	black = "#2E3440", --  nvim bg
+	black2 = "#343a46",
+	one_bg = "#373d49",
+	one_bg2 = "#464c58",
+	one_bg3 = "#494f5b",
+	grey = "#4b515d",
+	grey_fg = "#565c68",
+	grey_fg2 = "#606672",
+	light_grey = "#646a76",
+	red = "#BF616A",
+	baby_pink = "#de878f",
+	pink = "#d57780",
+	line = "#3a404c", -- for lines like vertsplit
+	green = "#A3BE8C",
+	vibrant_green = "#afca98",
+	blue = "#7797b7",
+	nord_blue = "#81A1C1",
+	yellow = "#EBCB8B",
+	sun = "#e1c181",
+	purple = "#aab1be",
+	dark_purple = "#B48EAD",
+	teal = "#6484a4",
+	orange = "#e39a83",
+	cyan = "#9aafe6",
+	statusline_bg = "#333945",
+	lightbg = "#3f4551",
+	lightbg2 = "#393f4b",
+	pmenu_bg = "#A3BE8C",
+	folder_bg = "#7797b7",
+}
+
+local kanagawa = {
+	white = "#DCD7BA",
+	darker_black = "#191922",
+	black = "#1F1F28", --  nvim bg
+	black2 = "#25252e",
+	one_bg = "#272730",
+	one_bg2 = "#2f2f38",
+	one_bg3 = "#363646",
+	grey = "#43434c",
+	grey_fg = "#4c4c55",
+	grey_fg2 = "#53535c",
+	light_grey = "#5c5c65",
+	red = "#E46876",
+	baby_pink = "#D27E99",
+	pink = "#c8748f",
+	line = "#272730", -- for lines like vertsplit
+	green = "#98BB6C",
+	vibrant_green = "#a3c677",
+	nord_blue = "#7E9CD8",
+	blue = "#7FB4CA",
+	yellow = "#FF9E3B",
+	sun = "#FFA066",
+	purple = "#a48ec7",
+	dark_purple = "#957FB8",
+	teal = "#7AA89F",
+	orange = "#fa9b61",
+	cyan = "#A3D4D5",
+	statusline_bg = "#24242d",
+	lightbg = "#33333c",
+	lightbg2 = "#2b2b34",
+	pmenu_bg = "#a48ec7",
+	folder_bg = "#7E9CD8",
+}
+
 -- Define the colour palette
 local palette = {
-	cyan = extract_highlight_colours("DraculaCyan").guifg,
-	green = extract_highlight_colours("DraculaGreen").guifg,
-	orange = extract_highlight_colours("DraculaOrange").guifg,
-	purple = extract_highlight_colours("DraculaPurple").guifg,
-	red = extract_highlight_colours("DraculaRed").guifg,
-	pink = extract_highlight_colours("DraculaPink").guifg,
+	-- cyan = extract_highlight_colours("DraculaCyan").guifg,
+	-- green = extract_highlight_colours("DraculaGreen").guifg,
+	-- orange = extract_highlight_colours("DraculaOrange").guifg,
+	-- purple = extract_highlight_colours("DraculaPurple").guifg,
+	-- red = extract_highlight_colours("DraculaRed").guifg,
+	-- pink = extract_highlight_colours("DraculaPink").guifg,
+	--
+	-- bg = extract_highlight_colours("Normal").guibg,
+	-- fg = extract_highlight_colours("Normal").guifg,
+	--
+	-- black2 = extract_highlight_colours("DraculaBgDark").guibg,
+	-- darker_black = extract_highlight_colours("DraculaBgDarker").guibg,
+	-- light = extract_highlight_colours("DraculaBgLight").guibg,
+	-- lighter = extract_highlight_colours("DraculaBgLighter").guibg,
+	cyan = kanagawa.cyan,
+	green = kanagawa.green,
+	orange = kanagawa.orange,
+	purple = kanagawa.purple,
+	red = kanagawa.red,
+	pink = kanagawa.pink,
 
-	bg = extract_highlight_colours("Normal").guibg,
-	fg = extract_highlight_colours("Normal").guifg,
+	bg = kanagawa.black,
+	fg = kanagawa.white,
 
-	black2 = extract_highlight_colours("DraculaBgDark").guibg,
-	darker_black = extract_highlight_colours("DraculaBgDarker").guibg,
-	light = extract_highlight_colours("DraculaBgLight").guibg,
-	lighter = extract_highlight_colours("DraculaBgLighter").guibg,
+	black2 = kanagawa.black2,
+	darker_black = kanagawa.darker_black,
+	light = kanagawa.lightbg,
+	lighter = kanagawa.lightbg2,
+
 	even_lighter = "#4d4f68",
 	lightest = "#626483",
 	dark_light = "#3a3c4e",
 	bg_light = "#343746",
 
-	light_red = "#FF6F6F",
-	light_cyan = "#A5FFFF",
-	light_green = "#6AFF95",
-	light_orange = "#FFD286",
+	light_red = nord.red,
+	light_cyan = nord.cyan,
+	light_green = nord.green,
+	light_orange = nord.orange,
+	light_grey = nord.light_grey,
 }
 
 -- git
@@ -92,6 +176,10 @@ highlight_group("GalaxylineHint", { guifg = palette.light_cyan, guibg = palette.
 highlight_group("GalaxylineWarn", { guifg = palette.light_orange, guibg = palette.bg })
 highlight_group("GalaxylineError", { guifg = palette.light_red, guibg = palette.bg })
 highlight_group("GalaxylineLsp", { guifg = palette.lightest, guibg = palette.bg })
+highlight_group("GalaxylinePercentIcon", { guifg = palette.bg, guibg = palette.light_green })
+highlight_group("GalaxylinePercentSep", { guifg = palette.light_green, guibg = palette.lightest })
+highlight_group("GalaxylineLineInfoIcon", { guifg = palette.bg, guibg = palette.light_red })
+highlight_group("GalaxylineLineInfoSep", { guifg = palette.light_red, guibg = palette.lightest })
 
 -- cursorline
 bg("ModesInsert", palette.cyan)
@@ -120,3 +208,6 @@ fg("FidgetTitle", palette.lightest)
 
 -- statusline
 bg("StatusLine", palette.bg)
+
+-- indent
+fg("IndentBlanklineContextChar", palette.lightest)
