8ad541ae44681cea61c6d2126f8082975ba0c132
Tom Hart <tommy-hart@hotmail.com>
2022-10-04 09:05:43 +0100

    fix dashboard in windows

--- lua/plugins/configs/dashboard-nvim.lua
+++ lua/plugins/configs/dashboard-nvim.lua
@@ -1,9 +1,6 @@
-local g = vim.g
+local db = require('dashboard')
 
-g.dashboard_disable_at_vimenter = 0
-g.dashboard_disable_statusline = 1
-g.dashboard_default_executive = "telescope"
-g.dashboard_custom_header = {
+db.custom_header = {
 	"                                   ",
 	"                                   ",
 	"                                   ",
@@ -115,10 +112,12 @@ g.dashboard_custom_header = {
 -- 	"                                                                    ",
 -- }
 
-g.dashboard_custom_section = {
-	a = { description = { "" }, command = "" },
-}
 
 local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 local lazy_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/opt", "*", 0, 1))
-g.dashboard_custom_footer = { "♡ " .. plugins_count .. " plugins loaded, " .. lazy_count .. " lazy. ♡" }
+local plugin_string = "♡ " .. plugins_count .. " plugins loaded, " .. lazy_count .. " lazy. ♡"
+
+db.custom_center = {
+  {icon = nil, desc = plugin_string}
+}
+db.custom_footer = {}
