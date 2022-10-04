a3a8b37ce64e2727eca4f6a14a09e5eb72b814d2
Tom Hart <tommy-hart@hotmail.com>
2022-10-04 21:14:39 +0100

    comment cmp-cmdline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -147,7 +147,7 @@ local plugins = {
       { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp', },
       { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp', },
       { "hrsh7th/cmp-path", after = "nvim-cmp", },
-      { "hrsh7th/cmp-cmdline", after = "nvim-cmp", },
+      -- { "hrsh7th/cmp-cmdline", after = "nvim-cmp", },
       { "onsails/lspkind-nvim", after = "nvim-cmp", },
       { 'saadparwaiz1/cmp_luasnip', after = 'LuaSnip', },
     },
