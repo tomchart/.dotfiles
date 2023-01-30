f2f980463e5e980ccf2604b038dd02b3689b0157
Tom Hart <tom@healthandcare.co.uk>
2023-01-30 12:10:00 +0000

    injection var rename, remap tele help

--- stow/.config/nvim/queries/php/injections.scm
+++ stow/.config/nvim/queries/php/injections.scm
@@ -2,6 +2,6 @@
 
 (assignment_expression
   left: (variable_name
-    (name) @_name (#match? @_name "SQL"))
+    (name) @_name (#match? @_name "sql"))
   right: (encapsed_string
     (string_value) @sql))
