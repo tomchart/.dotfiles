c1191bf5c50371c6cd437447824e3abe86d61b6e
Tom Hart <tom@healthandcare.co.uk>
2023-02-14 10:16:46 +0000

    switch back to rounded

--- stow/.config/nvim/after/plugin/lualine.lua
+++ stow/.config/nvim/after/plugin/lualine.lua
@@ -246,7 +246,7 @@ require("lualine").setup({
 	options = {
 		disabled_filetypes = { "dashboard" },
 		globalstatus = true,
-		section_separators = { left = "▊ ", right = "" },
+		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
 		theme = n,
 	},
