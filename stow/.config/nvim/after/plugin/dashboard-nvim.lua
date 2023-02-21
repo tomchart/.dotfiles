01d902a00e678c2049b424ede65b4a89a6d52aef
Tom Hart <tom@healthandcare.co.uk>
2023-02-21 13:48:58 +0000

    do not show projects on dashboard

--- stow/.config/nvim/after/plugin/dashboard-nvim.lua
+++ stow/.config/nvim/after/plugin/dashboard-nvim.lua
@@ -26,6 +26,9 @@ require("dashboard").setup({
     shortcut = {
       { desc = ' Update Packer', group = '@property', action = 'PackerSync', key = 'p' },
       { desc = ' Open Mason', group = '@text.literal', action = 'Mason', key = 'm' },
-    }
+    },
+    project = {
+      enable = false,
+    },
   }
 })
