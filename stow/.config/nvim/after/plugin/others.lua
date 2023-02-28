ece43c125b11bc5f4b7129fc95c9877242152214
Tom Hart <tom@healthandcare.co.uk>
2023-02-28 17:22:42 +0000

    autopairs cleanup

--- stow/.config/nvim/after/plugin/others.lua
+++ stow/.config/nvim/after/plugin/others.lua
@@ -1,13 +1,3 @@
--- autopairs
-require("nvim-autopairs").setup({
-  fast_wrap = {},
-})
-
--- cmp autopairs
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-
 -- leap
 local leap = require("leap")
 leap.setup({
