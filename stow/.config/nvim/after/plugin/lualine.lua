a545c9165d2f0a4d69fe928545658b2cce0e9fe2
Tom Hart <tom@healthandcare.co.uk>
2023-04-13 09:26:04 +0100

    sep

--- stow/.config/nvim/after/plugin/lualine.lua
+++ stow/.config/nvim/after/plugin/lualine.lua
@@ -201,10 +201,10 @@ require("lualine").setup({
 	options = {
 		disabled_filetypes = { "dashboard" },
 		globalstatus = true,
-		-- section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		-- section_separators = { left = "█", right = "█" },
 		-- section_separators = { left = "", right = " " },
-		section_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
 		theme = 'nordic',
 	},
