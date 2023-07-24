586ef5486f29a38d229865bdf84d852f2b37387b
Tom Hart <tom@healthandcare.co.uk>
2023-07-24 10:24:24 +0100

    tabwidth phpcbf

--- stow/.config/nvim/after/plugin/lsp.lua
+++ stow/.config/nvim/after/plugin/lsp.lua
@@ -11,7 +11,9 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.phpcbf,
+		null_ls.builtins.formatting.phpcbf.with({
+      extra_args = { "--tab-width=2" }
+    }),
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.diagnostics.selene,
 		null_ls.builtins.diagnostics.phpstan,
