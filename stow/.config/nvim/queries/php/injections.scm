0cb2e41d4ad29a31d38f5de1fdae58c7cd80f284
Tom Hart <tom@healthandcare.co.uk>
2023-08-01 10:39:21 +0100

    ts hl prop declaration sql

--- stow/.config/nvim/queries/php/injections.scm
+++ stow/.config/nvim/queries/php/injections.scm
@@ -5,3 +5,10 @@
     (name) @_name (#match? @_name "sql"))
   right: (encapsed_string
     (string_value) @sql))
+
+(property_element
+  (variable_name
+    (name) @_name (#match? @_name "sql"))
+  (property_initializer
+    (encapsed_string
+    (string_value) @sql)))
